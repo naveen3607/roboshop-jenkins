7efd21422e2186ea6540cc41748c11aa911a9dd3
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-22 16:05:16 -0400

    Jenkins Script

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,19 +1,22 @@
 def call() {
     node('workstation') {
-            stage('Compile Code') {
-                common.compile()
-            }
-            stage('Test') {
-                print "Hello"
-            }
-            stage('Code Quality') {
-                print "Hello"
-            }
-            stage('Code Security') {
-                print "Hello"
-            }
-            stage('Release') {
-                print "Hello"
-            }
+
+        sh 'find .'
+
+        stage('Compile Code') {
+           common.compile()
+        }
+        stage('Test') {
+           print "Hello"
+        }
+        stage('Code Quality') {
+           print "Hello"
+        }
+        stage('Code Security') {
+           print "Hello"
+        }
+        stage('Release') {
+           print "Hello"
+        }
     }
 }
