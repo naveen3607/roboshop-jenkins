50d4e0e62d33c653d864b1273472848a9273afcf
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-25 15:13:53 -0400

    Jenkins Script

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,6 +1,6 @@
 def compile() {
     if (env.codeType == "maven") {
-        sh 'mvn package'
+        sh '/root/maven/bin/mvn package'
     }
     if (env.codeType == "nodejs") {
         print "nodejs"
