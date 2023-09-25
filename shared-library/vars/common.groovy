31afb6e82502585df16725ec716eff9947f05d4d
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-25 15:57:00 -0400

    Jenkins Script

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,19 +1,16 @@
 def compile() {
+    if (env.codeType == "python" || env.codeType == "static") {
+        return "Return, Do not need any compilations"
+    }
     stage('Compile Code') {
         if (env.codeType == "maven") {
             sh '/home/centos/maven/bin/mvn package'
         }
         if (env.codeType == "nodejs") {
-            print "nodejs"
-        }
-        if (env.codeType == "python") {
-            print "python"
+            sh 'npm install'
         }
         if (env.codeType == "goLang") {
-            print "goLang"
-        }
-        if (env.codeType == "static") {
-            print "static"
+            sh 'go mod init dispatch ; go get ; go build'
         }
     }
 }
