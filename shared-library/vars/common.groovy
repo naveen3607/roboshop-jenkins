ac626e24bbe0646663debe30c4811c6c95d5d563
naveen3607 <naveenreddykallam3607@gmail.com>
2023-09-26 14:38:02 -0400

    Jenkins Script

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -34,8 +34,9 @@ def codequality() {
     stage('Code Quality') {
         env.sonarqubeuser = sh (script: 'aws ssm get-parameter --name "sonarqube.username" --query="Parameter.Value" |xargs', returnStdout: true).trim()
         env.sonarqubepass = sh (script: 'aws ssm get-parameter --name "sonarqube.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-        //wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: sonarqubepass]]])
-        sh 'sonar-scanner -X -D sonar.host.url=http://172.31.86.97:9000 -D sonar.login=${sonarqubeuser} -D sonar.password=${sonarqubepass} -D sonar.projectKey=${component} -D sonar.qualitygate.wait=true'
+        wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: sonarqubepass]]]) {
+            sh 'sonar-scanner -X -D sonar.host.url=http://172.31.86.97:9000 -D sonar.login=${sonarqubeuser} -D sonar.password=${sonarqubepass} -D sonar.projectKey=${component} -D sonar.qualitygate.wait=true'
+        }
     }
 }
 
